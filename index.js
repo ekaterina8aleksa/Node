7162529423eb4af9883f7c5a3339121ead336d71
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-12-06 18:31:13 +0200

    first changes

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
-const Contacts = require("./contacts");
-const argv = require("yargs").argv;
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+const morgan = require('morgan');
+const express = require('express');
+const contactsRouter = require('./api/contacts/router');
+const mongoose = require('mongoose');
 
-function invokeAction({ action, id, name, email, phone }) {
-    switch (action) {
-        case "list":
-            Contacts.listContacts().then((contacts) => console.table(contacts));
-            break;
 
-        case "get":
-            Contacts.getContactById(id).then((contact) =>
-                console.table(contact)
-            );
-            break;
+const runServer = async () => {
+await mongoose .connect(process.env.DB_URI, {useUnifiedTopology: true});
+console.log('Database connection successful');
 
-        case "add":
-            Contacts.addContact(name, email, phone).then((contact) =>
-                console.table(contact)
-            );
-            break;
+  const app = express();
 
-        case "remove":
-            Contacts.removeContact(id).then((contacts) =>
-                console.table(contacts)
-            );
-            break;
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/contacts', contactsRouter);
 
-        default:
-            console.warn("\x1B[31m Unknown action type!");
-    }
-}
+app.listen(PORT, () => console.log(`Server running on port: ${PORT} `));
 
-invokeAction(argv);
+};
+
+
+
+// const Contacts = require("./contacts");
+// const argv = require("yargs").argv;
+
+// function invokeAction({ action, id, name, email, phone }) {
+//     switch (action) {
+//         case "list":
+//             Contacts.listContacts().then((contacts) => console.table(contacts));
+//             break;
+
+//         case "get":
+//             Contacts.getContactById(id).then((contact) =>
+//                 console.table(contact)
+//             );
+//             break;
+
+//         case "add":
+//             Contacts.addContact(name, email, phone).then((contact) =>
+//                 console.table(contact)
+//             );
+//             break;
+
+//         case "remove":
+//             Contacts.removeContact(id).then((contacts) =>
+//                 console.table(contacts)
+//             );
+//             break;
+
+//         default:
+//             console.warn("\x1B[31m Unknown action type!");
+//     }
+// }
+
+// invokeAction(argv);
+
